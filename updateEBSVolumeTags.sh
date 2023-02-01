f08a3f6b8f2da6e64a6deeda8dab027e25ba5271
rchikatw <55136127+rchikatw@users.noreply.github.com>
2023-02-01 19:24:37 +0530

    Update volume name field

--- updateEBSVolumeTags.sh
+++ updateEBSVolumeTags.sh
@@ -6,7 +6,7 @@ usage() {
 
   Requirements:
     1. Backup of resources from the old cluster.
-    2. aws cli and jq installed.
+    2. aws cli,kubectl and jq installed.
     3. access to aws account from cli where volume's are available.
 
   USAGE: "./updateTags.sh "
@@ -48,10 +48,17 @@ do
   echo -e "Updating tags for volumeId "$volumeId
   region=$(cat backup/persistentvolumes/$pv | jq -r '.metadata .labels ."topology.kubernetes.io/region"')
   keyName=$(aws ec2 describe-volumes --volume-id $volumeId --filters Name=tag:kubernetes.io/created-for/pvc/namespace,Values=openshift-storage  --region $region --query "Volumes[*].Tags" | jq .[] | jq -r '.[]| select (.Value == "owned")|.Key')
+  backupKeyName=${keyName##*/}
   aws ec2 delete-tags --tags Key=$keyName --resources $volumeId --region $region
 
   #Update the tag with correct key
   keyName="kubernetes.io/cluster/"$(kubectl get machineset $(kubectl get machineset -n openshift-machine-api | awk 'NR!=1 {print}' | awk '{ print $1; exit }') -n openshift-machine-api -o json | jq -r '.metadata .labels ."machine.openshift.io/cluster-api-cluster"')
   aws ec2 create-tags --tags Key=$keyName,Value=owned --resources $volumeId --region $region
+
+  nameValue=$(aws ec2 describe-volumes --volume-id $volumeId --filters Name=tag:kubernetes.io/created-for/pvc/namespace,Values=openshift-storage  --region $region --query "Volumes[*].Tags" | jq .[] | jq -r '.[]| select (.Key == "Name")|.Value')
+  restoreKeyName=${keyName##*/}
+  restoreValue=${nameValue/$backupKeyName/$restoreKeyName}
+  aws ec2 create-tags --tags Key=Name,Value=$restoreValue --resources $volumeId --region $region
   echo -e "Updated tags for volumeId "$volumeId
+
 done
